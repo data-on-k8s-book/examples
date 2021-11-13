fa26ef0b42558695d9ed72749ac07eea97b12869
Jeffrey Carpenter <jeffreyscarpenter@cox.net>
2021-11-13 12:30:14 -0700

    initial draft of ch4 examples

---
+++ ch4-mysql-helm/README.md
@@ -0,0 +1,5 @@
+# MySQL Helm Chart Example 
+
+This example shows how to deploy MySQL on Kubernetes using a [Helm chart](https://github.com/bitnami/charts/tree/master/bitnami/mysql/) provided by Bitnami.
+
+This is a code example for the O'Reilly book [Managing Cloud-Native Data on Kubernetes](https://learning.oreilly.com/library/view/managing-cloud-native/9781098111380/).
---
+++ ch4-mysql-helm/openmysql.sh
@@ -0,0 +1,27 @@
+kubectl run -it --v=0 --rm --restart=Never --overrides='
+{
+  "spec": {
+    "containers": [
+      {
+        "name": "mysql-client",
+        "image": "mysql:5.7",       
+        "tty": true,
+        "stdin": true,
+        "command": ["mysql"],
+        "args": ["-h", "wordpress-mysql", "-u", "root", "-p$(DB_PASSWORD)"],
+        "env": [
+          {
+            "name": "DB_PASSWORD",
+            "valueFrom": {
+              "secretKeyRef": {
+                "name": "db-secret",
+                "key": "DB_PASSWORD"
+              }
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
+'  --image=mysql:5.7 mysql
